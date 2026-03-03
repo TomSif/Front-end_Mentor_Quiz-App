42a13c53d5cb33d99f85824047bcff7708efff54
TomSif <thomas.sifferle.programmer@gmail.com>
2026-03-03 09:04:08 +0100

    chore(app): single page app where currentView can be Home, Quiz, Results

--- quiz-app/src/App.jsx
+++ quiz-app/src/App.jsx
@@ -1,35 +1,41 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import Home from "./components/Home";
+import Quiz from "./components/Quiz";
+import Results from "./components/Results";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [currentView, setCurrentView] = useState("home");
+  const [score, setScore] = useState(0);
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1 className="text-red-500">Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  );
+  const views = {
+    home: (
+      <Home
+        onStart={() => {
+          setCurrentView("quiz");
+        }}
+      />
+    ),
+    quiz: (
+      <Quiz
+        onFinish={(finalScore) => {
+          setScore(finalScore);
+          setCurrentView("results");
+        }}
+      />
+    ),
+    results: (
+      <Results
+        score={score}
+        onRestart={() => {
+          setScore(0);
+          setCurrentView("home");
+        }}
+      />
+    ),
+  };
+
+  return <div>{views[currentView]}</div>;
 }
 
 export default App;
